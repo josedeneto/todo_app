cf8e64fceee17cbb79b3066c6072f3550dfb8cd0
josedeneto <josedenetojs@gmail.com>
2023-10-27 22:52:41 +0100

    Criação do pasta e ficheiro Binding

---
+++ lib/app/utils/bindings/app_binding.dart
@@ -0,0 +1,10 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+import 'package:flutter/material.dart';
+
+class AppBinding {
+  final Widget child;
+  AppBinding({
+    required this.child,
+  });
+}
