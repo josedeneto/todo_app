d400ab04d2c729cde71295dca1842a2de91fe3d7
josedeneto <josedenetojs@gmail.com>
2023-10-27 18:05:26 +0100

    Adicionei package intl para formatar horas

--- lib/app/app_todo.dart
+++ lib/app/app_todo.dart
@@ -1,9 +1,11 @@
 import 'package:app_todo/app/utils/theme/app_theme.dart';
+import 'package:app_todo/app/views/home/controller/home_controller.dart';
 import 'package:app_todo/app/views/settings/settings.page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
 import 'views/add_todo/add_todo_page.dart';
 import 'views/home/home_page.dart';
+
 
 class AppTodo extends StatelessWidget {
   const AppTodo({super.key});
